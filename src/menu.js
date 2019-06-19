43df1a8f7961cf057c8cfe8330092761f25bb649
James Patrick Keegan <atomicbomber.email@gmail.com>
2019-06-19 09:17:11 +0700

    Created project.

---
+++ src/menu.js
@@ -0,0 +1,23 @@
+const { Menu } = require('electron')
+
+const isMac = process.platform === 'darwin'
+
+const template = [
+    {
+        label: 'View',
+        submenu: [
+            { role: 'reload' },
+            { role: 'forcereload' },
+            { role: 'toggledevtools' },
+            { type: 'separator' },
+            { role: 'resetzoom' },
+            { role: 'zoomin' },
+            { role: 'zoomout' },
+            { type: 'separator' },
+            { role: 'togglefullscreen' }
+        ]
+    },
+]
+
+const menu = Menu.buildFromTemplate(template)
+module.exports = menu
